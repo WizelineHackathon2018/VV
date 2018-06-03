cc595c12ceac9d6d89107dbc4068a924e93d6018
omar3000 <ofa1995f14@hotmail.com>
2018-06-02 19:14:42 -0500

    se agrega script en php de ejemplo para aws

---
+++ vv_back/ejempo.php
@@ -0,0 +1,21 @@
+﻿<?php
+	include "conexion.php";
+	
+	$datos =array();
+	$i =0;
+	$consulta = "SELECT imagen FROM  imagenes";
+	$result = mysqli_query($conn, $consulta);
+	while($row = mysqli_fetch_assoc($result))  
+	{
+		   
+		   $datos[$i] = $row;
+		   $i++;
+	}
+	mysqli_close($conn);
+	echo json_encode($datos);
+	
+
+	
+
+
+?>
